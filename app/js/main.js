a772104c3709826afd39add5e37a778d6b1cbe01
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-12-28 02:55:33 +0200

    main-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,190 @@
 
 $(document).ready(function(){
 
-// $('#slider-1').bxSlider({
-// 		controls: true, 
-// 		pager: false,
-// 		slideWidth: 940,
-// 		minSlides: 4,
-// 		maxSlides: 4,
-// 		moveSlides: 1
-// 	});
 
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  center: true,
-  autoHeight: true,
-  touchDrag: false,
-  mouseDrag: false,
-  dotsEach: true,
-  responsiveClass:true,
-  responsiveClass:true,
-  navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
-  responsive:{
-    0:{
-      stagePadding: 0,
-      items:1
-    },
-    600:{
-      stagePadding: 100,
-      items:1
-    },
-    1000:{
-      stagePadding: 200,
-      items:1
-    }
-  }
+$('.m-h-2').matchHeight({
+	byRow: false 
+});
+$('.wrapp-service > div').matchHeight();
+
+
+
+
+
+$(window).on('load', function() {
+
+
+
+	$('.section-carousel-1').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:false,
+		center: true,
+		autoHeight: true,
+		touchDrag: false,
+		mouseDrag: false,
+		dots: true,
+		dotsEach: true,
+		responsiveClass:true,
+		responsiveClass:true,
+		navText: ['',''],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+
+	
+	var widthItem = $(".section-carousel-1 .owl-item").width();
+	$(".owl-item .item").css("width", widthItem +1)
+
+
+
+
+	var owl = $('.section-carousel-2');
+	owl.owlCarousel({
+		loop:true,
+		margin:0,
+		nav:false,
+		center: true,
+		autoHeight: true,
+		touchDrag: false,
+		mouseDrag: false,
+		dots: false,
+		dotsEach: true,
+		responsiveClass:true,
+		responsiveClass:true,
+		navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+		responsive:{
+			0:{
+				items:1,
+				stagePadding: 0
+			},
+			600:{
+				items:1,
+				stagePadding: 0
+			},
+			1000:{
+				items:1,
+				stagePadding: 0
+			},
+			1050:{
+				items:1,
+				stagePadding: 0
+			},
+			1200:{
+				items:1,
+				stagePadding: 80
+			},
+			1240:{
+				items:1,
+				stagePadding: 20
+			},
+			1300:{
+				items:1,
+				stagePadding: 45
+			},
+			1350:{
+				items:1,
+				stagePadding: 65
+			},
+			1400:{
+				items:1,
+				stagePadding: 85
+			},
+			1500:{
+				items:1,
+				stagePadding: 120
+			},
+			1600:{
+				items:1,
+				stagePadding: 160
+			},
+			1700:{
+				items:1,
+				stagePadding: 210
+			},
+			1800:{
+				items:1,
+				stagePadding: 250
+			},
+			2000:{
+				items:1,
+				stagePadding: 330
+			},
+			2200:{
+				items:1,
+				stagePadding: 400
+			}
+		}
+	});
+
+
+
+	$('.customNextBtn').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+	$('.customPrevBtn').click(function() {
+		owl.trigger('prev.owl.carousel', [300]);
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$('.slider-3').owlCarousel({
+		loop:true,
+		nav:false,
+		// center: true,
+		margin: 0,
+		autoHeight: false,
+		touchDrag: false,
+		mouseDrag: false,
+		dots: true,
+		dotsEach: true,
+		responsiveClass:true,
+		responsiveClass:true,
+		navText: ['',''],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			1100:{
+				items:2
+			},
+			1200:{
+				items:3
+			},
+			1300:{
+				items:5
+			}
+		}
+	});
+
+
+	$('.slider-3 .item').matchHeight();
+
 });
 
 
-center: true,
-// activeClass: 'qqq',
-touchDrag: false,
-mouseDrag: false,
-responsiveClass:true,
 
  //    $("a.modal-form").fancybox({
  //       'hideOnContentClick': true,
@@ -74,10 +215,10 @@ responsiveClass:true,
 // });
 
 $('input,textarea').focus(function(){
- $(this).data('placeholder',$(this).attr('placeholder'))
- .attr('placeholder','');
+	$(this).data('placeholder',$(this).attr('placeholder'))
+	.attr('placeholder','');
 }).blur(function(){
- $(this).attr('placeholder',$(this).data('placeholder'));
+	$(this).attr('placeholder',$(this).data('placeholder'));
 });
 
 });
